4e1ffa120675d7c7d686f8a0a410b21e8fe58d11
habiba <arnoba2012@gmail.com>
2023-03-16 18:01:12 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * printf: prints a string followed by a new line.
+ * Return: 0
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
