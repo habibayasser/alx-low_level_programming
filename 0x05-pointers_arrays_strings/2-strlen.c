8d8362d3e900b13c4a98073d5fd58f759719cce3
habiba <arnoba2012@gmail.com>
2023-03-27 10:38:00 -0700

    done

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,16 @@
 /**
  * _strlen - return the lenght of the string
  * @s: pointer to string
+ *
  * return: the lenght of the string
  */
 int _strlen(char *s)
 {
 	int lenght=0;
-	while(*s!='\0'){
+	while(*s != '\0')
+	{
 		lenght++;
 		s++;
 	}
-	retrun(lenght)
+	retrun(lenght);
 }
