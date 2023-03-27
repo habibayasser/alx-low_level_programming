a8a79476816527d3e7f817aa2f57fb8ed1cb7a95
habiba <arnoba2012@gmail.com>
2023-03-27 11:18:08 -0700

    done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - print the string in reverse followed by new line
+ * @s: pointer to string
+ *
+ * return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int lenght = 0;
+	
+	while(s[lenght])
+	{
+		lengh++;
+	}
+	for(i = lenght - 1;i >= 0;i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n'')
+}
