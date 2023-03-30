5f69e194f6fde96699498180c53de0bf351a18bb
habiba <arnoba2012@gmail.com>
2023-03-30 11:56:01 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,12 +9,12 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 	int len = 0;
 
 	while (dest[i] != '\0')
 		i++;
-	while(src[len] != '\0')
+	while (src[len] != '\0')
 	{
 		dest[i] = src[len];
 		i++;
