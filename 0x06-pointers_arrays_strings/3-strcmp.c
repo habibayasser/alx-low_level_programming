daa27c1bf77ff50b96de83df4a154ed8d11e04ab
habiba <arnoba2012@gmail.com>
2023-03-30 13:19:08 -0700

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,6 @@
  * return: If str1 < str2, the negative difference of
  * the first unmatched characters
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	while ((*s1 && *s2) && (*s1 == *s2))
@@ -16,5 +15,7 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-	return (*S1 - *s2);
+
+	return (*s1 - *s2);
 }
+
