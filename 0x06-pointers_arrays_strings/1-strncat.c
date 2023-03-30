aee5a31be67f66fbdd0b4a3648d4bc04017fe749
habiba <arnoba2012@gmail.com>
2023-03-30 12:47:39 -0700

    done

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,16 +10,11 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[i++] != '\0')
-	{
-		j++;
-	}
-	for (i = 0; src[i] && i <n; i++)
-	{
-		dest[j] = src[i];
-	}
-	
-	return(dest);
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
