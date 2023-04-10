d8a548db4583755d3552890a6f49205e61632b83
habiba <arnoba2012@gmail.com>
2023-04-10 15:52:29 -0700

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ * main - print number
+ * argc: elemant counter
+ * argv: elemant value
+ *
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
