483f9bfe132d66d0d7450a0a0c67ca58391a3c5e
habiba <arnoba2012@gmail.com>
2023-04-10 16:16:14 -0700

    done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+/**
+ * main - print all arguments it receives
+ * @argc: elemant counter
+ * @argv: argument value
+ *
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%S\n", argv[i]);
+	}
+	return (0);
+}
