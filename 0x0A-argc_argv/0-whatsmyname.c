b4f8b52b18d113c01bbef1ca9485dc39f07e560f
habiba <arnoba2012@gmail.com>
2023-04-10 15:43:55 -0700

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ * main - print name
+ * @argc: elemant counter
+ * @argv: element value
+ *
+ * Return: zero
+ */
+
+int main (int argc ,char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
