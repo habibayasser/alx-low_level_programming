527bb61943d3f6f760899e3603efe725333ee573
habiba <arnoba2012@gmail.com>
2023-04-11 11:52:07 -0700

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -33,7 +33,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i]; i++)
 		con[conc_len++] = s1[i];
 
-	for (i = 0; s2[]; i++)
+	for (i = 0; s2[i]; i++)
 		con[conc_len++] = s2[i];
 
 	return (con);
