f9d1b290b53f1359790fb7ae573ae0642bbbc7fa
habiba <arnoba2012@gmail.com>
2023-04-17 05:11:23 -0700

    d

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,22 +21,34 @@ int _strlen(char *str)
 	return (len);
 }
 
-
-
+/**
+ * _strcopy - Copies a string pointed to by src, including the
+ *            terminating null byte, to a buffer pointed to by dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ *
+ * Return: The pointer to dest.
+ */
 char *_strcopy(char *dest, char *src)
 {
-	int i= 0;
+	int index = 0;
 
-	for (i = 0; src[i]; i++)
-		dest[i] = src[i];
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
 
-	dest[i] = '\0';
+	dest[index] = '\0';
 
 	return (dest);
 }
 
-
-
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: The new struct dog.
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
