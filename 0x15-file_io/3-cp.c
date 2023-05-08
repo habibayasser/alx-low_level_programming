d2d849a345340f5f5a2cfcea15d94bad6b7a967c
habiba <habibayasser100@gmail.com>
2023-05-08 12:44:56 -0700

    done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,6 @@ void close_file(int fd);
  *
  * Return: A pointer to the newly-allocated buffer.
  */
-
 char *create_buffer(char *file)
 {
 	char *buffer;
@@ -24,8 +23,7 @@ char *create_buffer(char *file)
 		exit(99);
 	}
 
-	return(buffer);
-
+	return (buffer);
 }
 
 /**
@@ -34,11 +32,11 @@ char *create_buffer(char *file)
  */
 void close_file(int fd)
 {
-	int cloz;
+	int c;
 
-	cloz = close(fd);
+	c = close(fd);
 
-	if (cloz == -1)
+	if (c == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
@@ -58,8 +56,8 @@ void close_file(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int from,_f to_f, re, wr;
-	char *list;
+	int from, to, r, w;
+	char *buffer;
 
 	if (argc != 3)
 	{
@@ -67,37 +65,37 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	list = create_buffer(argv[2]);
-	from_f = open(argv[1], O_RDONLY);
-	re = read(from_f, list, 1024);
-	to_f = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from_f == -1 || re == -1)
+		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
-			free(list);
+			free(buffer);
 			exit(98);
 		}
 
-		wr = write(to_f, list, re);
-		if (to_f == -1 || wr == -1)
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
-			free(list);
+			free(buffer);
 			exit(99);
 		}
 
-		re = read(from_f, list, 1024);
-		to_f = open(argv[2], O_WRONLY | O_APPEND);
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (re > 0);
+	} while (r > 0);
 
-	free(list);
-	close_file(from_f);
-	close_file(to_f);
+	free(buffer);
+	close_file(from);
+	close_file(to);
 
 	return (0);
 }
